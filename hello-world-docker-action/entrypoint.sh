17c8231bd97d70e41630e6e523f9e13dc38ba67b
Fieseler, Mattis <Mattis.Fieseler@wago.com>
2024-03-28 10:22:42 +0100

    try converting xml to puml

--- hello-world-docker-action/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -l
-
-ls ./hello-world-docker-action -la
-apt install python3 -y
-python3 test.py
-
-echo "Hello $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
--- hello-world-docker-action/test.py
+++ hello-world-docker-action/test.py
@@ -1,2 +1,24 @@
+import os, xml.etree.ElementTree as ET
 
-print("hello world!")
+print(os.listdir())
+tree = ET.parse("core-items.xml")
+
+root = tree.getroot()
+
+itemtypes = root.find("itemtypes")
+for itemtype in itemtypes:
+    attributes = itemtype.find("attributes")
+    with open("core.puml", "w+") as f:
+        f.write("@startuml\n")
+        f.write("skinparam class {\n")
+        f.write("    BackgroundColor White\n")
+        f.write("    BorderColor Black\n")
+        f.write("    ArrowColor Black\n")
+        f.write("}\n")
+        f.write(f"package core" + r" {\n")
+        f.write(f"    class {itemtype.get('code')}" + r" {\n")
+        for attribute in attributes:
+            f.write(f"        {attribute.get('qualifier')}: {attribute.get('type')}\n")
+        f.write("    }\n")
+        f.write("}\n")
+        f.write("@enduml\n")
